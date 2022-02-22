1b40f803e1b4c586ef3b6aa2fc328985a60f8313
skdoqj <80323356+skdoqj@users.noreply.github.com>
2022-02-22 22:00:19 +0900

    3

--- src/component/main_page/Filter.js
+++ src/component/main_page/Filter.js
@@ -1,125 +1,159 @@
-import appStyles from '../../App.module.css'
-import mainStyles from './Main.module.css'
+// import appStyles from '../../App.module.css'
+import mainStyles from "./Main.module.css";
+import Card from "../Card.js";
 
-import { Link } from 'react-router-dom';
-import { Form } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import { Form } from "react-bootstrap";
 
-import Box from '@mui/material/Box';
-import Slider from '@mui/material/Slider';
-
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
 
 export default function Filter() {
-    const alcoholMarks = [
-        {
-            value: 1,
-            label: '😊',
-        },
-        {
-            value: 3,
-            label: '🐵',
-        },
-        {
-            value: 5,
-            label: '🐶',
-        },
-    ];
-    const dryMarks = [
-        {
-            value: 1,
-            label: '🍯',
-            
-        },
-        {
-            value: 3,
-            label: '🍷',
-        },
-        {
-            value: 5,
-            label: '🚬',
-        },
-    ];
+  // 함수
+  // -> 카드들 생겨요
+  // 가짜 html카드 ~~~~~~~
+  const alcoholMarks = [
+    {
+      value: 1,
+      label: "😊",
+    },
+    {
+      value: 3,
+      label: "🐵",
+    },
+    {
+      value: 5,
+      label: "🐶",
+    },
+  ];
+  const dryMarks = [
+    {
+      value: 1,
+      label: "🍯",
+    },
+    {
+      value: 3,
+      label: "🍷",
+    },
+    {
+      value: 5,
+      label: "🚬",
+    },
+  ];
 
-    return (
-        <div className={mainStyles.filterSection}>
-            <h2>Filter Section</h2>
-            {/* 필터+검색결과 */}
-            <div className={mainStyles.filterContainer}>
-
-                {/* 필터 */}
-                <div className={mainStyles.filter}>
-                    <div className="alcohol">
-                        얼마나 취할래
-                        <div className={mainStyles.slider}>
-                            <Box sx={{ width: 200 }}>
-                                <Slider min={1} max={5}
-                                    marks={alcoholMarks} 
-                                    valueLabelDisplay="on"
-/>
-                            </Box>
-                        </div>
-                    </div>
-                    <div className="dry">
-                        Sweet or Dry?
-                        <div className={mainStyles.slider}>
-                            <Box sx={{ width: 200 }}>
-                                <Slider min={1} max={5}
-                                    marks={dryMarks}
-                                    valueLabelDisplay="on"
- />
-                            </Box>
-                        </div>
-                    </div>
-                    <div className="tastingNote">
-                        테이스팅 노트
-                        <div id="checkBoxList">
-                            <input type="checkbox" />프레시
-                            <input type="checkbox" />과일
-                            <input type="checkbox" />아이셔
-                            <input type="checkbox" />허브
-                            <input type="checkbox" />아이써
-                        </div>
-                    </div>
-                    <div className="base">
-                        베이스
-                        <Form>
-                            <Form.Check label="진" />
-                            <Form.Check label="럼" />
-                            <Form.Check label="위스키" />
-                            <Form.Check label="데킬라" />
-                            <Form.Check label="보드카" />
-                            <Form.Check label="브랜디" />
-                        </Form>
-                    </div>
-                    <button id="filterBtn">검색</button>
-                </div>
-
-                {/* 결과 칵테일 카드 */}
-                <div className={mainStyles.result}>
-                    <Link to="/desc">
-                        <div className={appStyles.card}>
-                            칵테일
-                        </div>
-                    </Link>
-
-                    <Link to="/desc">
-                        <div className={appStyles.card}>
-                            칵테일
-                        </div>
-                    </Link>
-                    <Link to="/desc">
-                        <div className={appStyles.card}>
-                            칵테일
-                        </div>
-                    </Link>
-                    <Link to="/desc">
-                        <div className={appStyles.card}>
-                            칵테일
-                        </div>
-                    </Link>
-
-                </div>
+  return (
+    <div className={mainStyles.filterSection}>
+      <h2>Filter Section</h2>
+      {/* 필터+검색결과 */}
+      <div className={mainStyles.filterContainer}>
+        {/* 필터 */}
+        <div className={mainStyles.filter_box}>
+          <div className="alcohol">
+            얼마나 취할래
+            <div className={mainStyles.slider}>
+              <Box sx={{ width: 200 }}>
+                <Slider
+                  min={1}
+                  max={5}
+                  marks={alcoholMarks}
+                  valueLabelDisplay="on"
+                />
+              </Box>
             </div>
+          </div>
+          <div className="dry">
+            Sweet or Dry?
+            <div className={mainStyles.slider}>
+              <Box sx={{ width: 200 }}>
+                <Slider
+                  min={1}
+                  max={5}
+                  marks={dryMarks}
+                  valueLabelDisplay="on"
+                />
+              </Box>
+            </div>
+          </div>
+          <div className="tastingNote">
+            테이스팅 노트
+            <div id={mainStyles.checkBoxList}>
+              <label>
+                <input type="checkbox" />
+                프레시
+              </label>
+              <label>
+                <input type="checkbox" />
+                과일
+              </label>
+              <label>
+                <input type="checkbox" />
+                아이셔
+              </label>
+              <label>
+                <input type="checkbox" />
+                허브
+              </label>
+              <label>
+                <input type="checkbox" />
+                아이써
+              </label>
+            </div>
+          </div>
+          <div className="base">
+            베이스
+            <div id={mainStyles.checkBoxList}>
+              <label>
+                <input type="checkbox" />진
+              </label>
+              <label>
+                <input type="checkbox" />럼
+              </label>
+              <label>
+                <input type="checkbox" />
+                위스키
+              </label>
+              <label>
+                <input type="checkbox" />
+                데킬라
+              </label>
+              <label>
+                <input type="checkbox" />
+                보드카
+              </label>
+            </div>
+          </div>
+          <button id={mainStyles.filterBtn}>검색</button>
         </div>
-    );
 
+        <Card />
+        {/* 결과 칵테일 카드 */}
+        {/* <div className={mainStyles.result}>
+                    <Link to="/desc">
+                        <div className={appStyles.card}>
+                            <img className={mainStyles.imgCocktail} src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
+                            칵테일
+                        </div>
+                    </Link>
+
+                    <Link to="/desc">
+                        <div className={appStyles.card}>
+                            칵테일
+                        </div>
+                    </Link>
+                    <Link to="/desc">
+                        <div className={appStyles.card}>
+                            칵테일
+                        </div>
+                    </Link>
+                    <Link to="/desc">
+                        <div className={appStyles.card}>
+                            칵테일
+                        </div>
+                    </Link> 
+                    
+
+                </div> */}
+      </div>
+    </div>
+  );
 }
--- src/component/main_page/Top100.js
+++ src/component/main_page/Top100.js
@@ -1,11 +1,75 @@
 import mainStyles from './Main.module.css'
+import styles from '../Card.module.css'
 
-export default function Top100(){
+import {useState} from "react";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { Navigation, Pagination, Autoplay } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation'; //좌우 화살표
+import 'swiper/css/pagination'; //하단 이동 버튼
+import 'swiper/css/autoplay'; //자동 넘기기
+
+
+export default function Top100() {
+    // let swiper = new Swiper()
+    let [slide, setSlide] = useState(mainStyles.slide)
+    function changeSlideClass() {
+        setSlide(mainStyles.active)
+    }
+
     return (
-    <div className={mainStyles.Top100}>
-        <h1>World's Top 100</h1>
-        <h1>Cocktils</h1>
-        <div className={mainStyles.box}></div>
-    </div>
+        <div className={mainStyles.Top100}>
+            <h1>World's Top 100</h1>
+            <h1>Cocktails</h1>
+
+            <Swiper className={mainStyles.swiper}
+                // install Swiper modules
+                modules={[Navigation, Pagination, Autoplay]}
+                spaceBetween={40}
+                slidesPerView={3}
+                centeredSlides={'true'}
+                grabCursor={'true'}
+                // autoplay={{
+                //     delay: 2000,
+                //     disableOnInteraction: false,
+                // }}
+                navigation={true}
+                pagination={{ clickable: true }}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <div className={mainStyles.slider_wrapper}>
+                    <SwiperSlide>
+                        {({ isActive }) => (
+                            <div> {isActive ? changeSlideClass() : 'not active'}</div>
+                            // classname 바꾸기
+                        )}
+                    </SwiperSlide>
+                    <SwiperSlide className={slide}>
+                        <img src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
+                    </SwiperSlide>
+                    <SwiperSlide className={slide}>
+                        <img src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
+                    </SwiperSlide>
+                    <SwiperSlide className={mainStyles.slide}>
+                        <img src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
+                    </SwiperSlide>
+                    <SwiperSlide className={mainStyles.slide}>
+                        <img src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
+                    </SwiperSlide>
+                    <SwiperSlide className={mainStyles.slide}>
+                        <img src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
+                    </SwiperSlide>
+                </div>
+
+            </Swiper>
+        </div>
+
+
     );
 }
