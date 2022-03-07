8d5cd72e4d23043d849b44229a148957f03975bd
skdoqj <80323356+skdoqj@users.noreply.github.com>
2022-03-08 03:29:52 +0900

    5 filter css

--- src/component/Card.js
+++ src/component/Card.js
@@ -4,6 +4,33 @@ export default function Card() {
     
   return (
     <div className={styles.cardContainer}>
+
+      <div className={styles.imgContainer}>
+        <img className={styles.imgCocktail} src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
+        <h3 className={styles.cocktailName}> Cocktail </h3>
+      </div>
+
+
+      <div className={styles.imgContainer}>
+        <img className={styles.imgCocktail} src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
+        <h3 className={styles.cocktailName}> Cocktail </h3>
+      </div>
+
+      <div className={styles.imgContainer}>
+        <img className={styles.imgCocktail} src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
+        <h3 className={styles.cocktailName}> Cocktail </h3>
+      </div>
+
+      <div className={styles.imgContainer}>
+        <img className={styles.imgCocktail} src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
+        <h3 className={styles.cocktailName}> Cocktail </h3>
+      </div>
+
+      <div className={styles.imgContainer}>
+        <img className={styles.imgCocktail} src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
+        <h3 className={styles.cocktailName}> Cocktail </h3>
+      </div>
+
       <div className={styles.imgContainer}>
         <img className={styles.imgCocktail} src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
         <h3 className={styles.cocktailName}> Cocktail </h3>
