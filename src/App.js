48165e34776299323c7d24635b8a228eab5c2bb1
skdoqj <80323356+skdoqj@users.noreply.github.com>
2022-05-13 00:21:11 +0900

    5.13

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ import Login from './component/Login.js';
 import Home from './component/Home.js';
 import Desc from './component/Desc.js';
 import Oauth from './Oauth.js';
-
+// 리덕스
+import { connect } from "react-redux";
+import store, { get } from './store';
+import { createStore } from 'redux';
+import { Provider, useSelector, useDispatch } from 'react-redux';
 
 //라우터
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
@@ -20,56 +24,75 @@ import { useEffect, useState } from "react";
 
 import { code } from './Oauth.js'
 
-// 리액트 리덕스
-import { createStore } from 'redux';
-import { Provider, useSelector, useDispatch } from 'react-redux'; 
+// context
+import { createContext } from 'react';
 
+import { APIContext } from './context/APIContext'
 
 const URL = 'http://localhost:5000'
 
-function App() {
+function App({ }) {
     let [cocktailsInfo, setcocktailsInfo] = useState([]);
 
     // API GET
     const getCocktails = async () => {
         const { data: { all_cocktails } } = await axios.get(`${URL}/cocktails`);
         const cocktails = JSON.parse(all_cocktails)
-        setcocktailsInfo(cocktails);        
+
+        setcocktailsInfo(cocktails);
+        
+
+        //redux store로 보내기      
+        // getArray(cocktails); 
+        // console.log(store.getState());    
     };
-    
-    console.log(cocktailsInfo);
+
+
+
+
+    // console.log(cocktailsInfo);
     // console.table(cocktailsInfo);
-    
+
     // 리덕스
-    function reducer(state = cocktailsInfo, action){
+    function reducer(state = cocktailsInfo, action) {
         return state
     }
-    let store = createStore(reducer)
+
+    const store = createStore(reducer);
+
 
     useEffect(() => {
-            getCocktails()
-    },[]);
+        getCocktails()
+    }, []);
 
 
-        return (  
-            <BrowserRouter>
+    return (
+        <BrowserRouter>
             <Provider store={store}>
-                <div className="App">
-                    <Header />
-                    <div id="pages">
-                        <Routes>
-                            <Route path="/" element={<Main />} /> //Top100+Filter
-                            <Route path="/find" element={<Find />} /> //칵테일 검색(모든칵테일)
-                            <Route path="/home" element={<Home />} /> //고향칵테일
-                            <Route path="/storage" element={<Storage />} /> //내 칵테일
-                            <Route path="/desc:id" element={<Desc />} />
-                            <Route path="/oauth" element={<Oauth />} />
-                        </Routes>
+                <APIContext.Provider value={cocktailsInfo}>
+                    <div className="App">
+                        <Header />
+                        <div id="pages">
+                            <Routes>
+                                <Route path="/" element={<Main />} /> //Top100+Filter
+                                <Route path="/find" element={<Find />} /> //칵테일 검색(모든칵테일)
+                                <Route path="/home" element={<Home />} /> //고향칵테일
+                                <Route path="/storage" element={<Storage />} /> //내 칵테일
+                                <Route path="/desc:id" element={<Desc />} />
+                                <Route path="/oauth" element={<Oauth />} />
+                            </Routes>
+                        </div>
                     </div>
-                </div>
-                </Provider>
-            </BrowserRouter>
-        );
-    }
+                </APIContext.Provider>
+            </Provider>
+        </BrowserRouter>        
+    );
+}
 
+// function mapDispatchProps(dispatch) {
+//     return {
+//         getArray: array => dispatch(get(array))}
+// }
+
+// export default connect (null, mapDispatchProps) (App);
 export default App;
