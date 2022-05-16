0f1a136ed583c8ff7705ebbd36b6fb05e5b3362b
skdoqj <80323356+skdoqj@users.noreply.github.com>
2022-05-16 18:28:08 +0900

    5.16

--- src/component/Desc.js
+++ src/component/Desc.js
@@ -1,76 +1,77 @@
-import styles from './Desc.module.css'
-import { useSelector } from 'react-redux';
+import styles from "./Desc.module.css";
+import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 //contect
-import { useContext } from 'react';
-import { APIContext } from '../context/APIContext';
-
+import { useContext } from "react";
+import { APIContext } from "../context/APIContext";
 
 function Desc() {
-    //주소에서 id받기
-    const { id } = useParams();
-    console.log(id);
+  //주소에서 id받기
+  const { id } = useParams();
+  console.log(id);
 
-    // 리덕스로 api받기
-    // const cocktail_api = useSelector((state) => state)
+  // 리덕스로 api받기
+  // const cocktail_api = useSelector((state) => state)
 
-    //context API받기
-    const API = useContext(APIContext);
+  //context API받기
+  const API = useContext(APIContext);
 
-    const n = API.findIndex(item => `:${item._id.$oid}` === id)
-    console.log(n)
+  const n = API.findIndex((item) => `:${item._id.$oid}` === id);
+  console.log(n);
 
-    const name = API[n].name
-    const img = API[n].img
-    const base = API[n].base
-    const booziness = API[n].booziness
-    const sweetness = API[n].sweetness
-    const flavor = API[n].flavor
-    const hashtag = API[n].hashtag
-    const howtomake = API[n].korean
-    const ingredients = API[n].ingredients
+  const name = API[n].name;
+  const img = API[n].img;
+  const base = API[n].base;
+  const booziness = API[n].booziness;
+  const sweetness = API[n].sweetness;
+  const flavor = API[n].flavor;
+  const hashtag = API[n].hashtag;
+  const howtomake = API[n].korean;
+  const ingredients = API[n].ingredients;
 
-
-
-
-    return (
-        <div id="desc">
-            {/* 이미지+설명 */}
-            <div>
-                <div className={styles.box}>
-                    <img src={img}></img>
-                </div>
-
-                <span></span>
-            </div>
-            {/* 레시피 */}
-            <div>
-                <h1>{name}
-                </h1>
-                <span>#해시태그 {hashtag}</span>
-                {/* 알콜 당도 정보 */}
-                <div id="info">
-                    <div>베이스: {base}</div>
-                    <div>맛: {flavor}</div>
-                    <div>알콜세기 {booziness}</div>
-                    <div>당도 {sweetness}</div>
-                </div>
-                {/* 재료 */}
-                <div>
-                    <ul>
-                        {ingredients.map((item) =>
-                            <li>{item.재료} {item.양}</li>)}
-                    </ul>
-                    <h4>믹스방법</h4>
-                    <p>{howtomake}</p>
-                </div>
-            </div>
-
+  return (
+    <div id="desc" className={styles.container}>
+      {/* 이미지+설명 */}
+      <div>
+        <div className={styles.box}>
+          <img src={img}></img>
         </div>
-    );
+
+        <span></span>
+      </div>
+      {/* 레시피 */}
+      <div className={styles.text_box}>
+        <h1>{name}</h1>
+        {/* 해시태그 없으면 안보이기 */}
+        {hashtag == "no info" ? "" : <span>#해시태그 {hashtag}</span>}
+
+        {/* 알콜 당도 정보 */}
+        <div id="info" className={styles.info}>
+          <div className={styles.base}>베이스 {base}</div>
+          <div>맛 {flavor}</div>
+          <div>알콜세기 {booziness}</div>
+          <div>당도 {sweetness}</div>
+        </div>
+        {/* 재료 */}
+        <div>
+          <ul className={styles.ingredients}>
+            {ingredients.map((item) => (
+              <li>
+                {item.재료} {item.양}
+              </li>
+            ))}
+          </ul>
+          <div className={styles.mix}>
+            <h4>믹스방법</h4>
+            <p>{howtomake}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Desc;
