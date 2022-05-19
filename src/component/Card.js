78d92a37f0eb49299c018ede969690e889edccc8
jakebaebaek <87072502+jakebaebaek@users.noreply.github.com>
2022-05-19 21:08:53 +0900

    Add files via upload

--- src/component/Card.js
+++ src/component/Card.js
@@ -5,64 +5,49 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import StarBorderIcon from '@mui/icons-material/StarBorder';
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from "axios";
-
-import { connect, useSelector } from "react-redux";
-import { Construction } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
 
 function Card({ id, img, name }) {
   // 임시 로컬주소
   const URL = 'http://localhost:5000'
-  const userID = useSelector((state) => state)
-
   const [checked, setChecked] = useState(false)
+  const MembersID = useSelector(state => state);
+
 
   const onClick = () => {
-
-    setChecked((prev) => !prev);
-    
-    console.log(userID)
-    console.log(name)
-    console.log(checked)
-    
-    
-
+    setChecked(current => !current);
+      console.log(MembersID)
     axios.post(
       `${URL}/favourite`,
       {
-        member_id_give: userID,
-        name_give: name,
-        checked_give: (checked ? 1 : 0)
+          member_id_give: MembersID,
+          name_give: name,
+          checked_give: (checked ? 1 : 0)
       })
       .then((res) => {
-        console.log(res);
-        alert("성공");
+          console.log(res);
+          alert("통신성공");
       })
       .catch((error) => {
-        console.error(error);
-
+          // console.log(error);
+          console.error(error);
+          
       });
   }
 
-  const [hover, setHover] = useState(0);
-
-
   return (
     <div className={cardStyles.card}>
-      {/* {hover == 0 ? "" : <StarBorderIcon onClick={onClick} className={cardStyles.star_icon} />}       */}
-      <StarBorderIcon onClick={onClick} className={cardStyles.star_icon} />
+      <StarBorderIcon onClick={onClick} className={cardStyles.star_icon}/>
       <Link to={`/desc:${id}`}>
-        <div
-          onMouseOver={() => setHover(1)}
-          onMouseOut={() => setHover(0)}
-          className={cardStyles.imgContainer}>
+        <div className={cardStyles.imgContainer}>
           <img className={cardStyles.imgCocktail} src={img} />
           <h3 className={cardStyles.cocktailName}>{name}</h3>
         </div>
       </Link>
 
-
+      
 
       {/* 
       <div className={styles.imgContainer}>
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,154 +1,173 @@
-import styles from "./Header.module.css";
-import { Link } from "react-router-dom";
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
 
 // import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 // import { Email, Style } from '@material-ui/icons';
 // import { style } from '@mui/system';
 
 import axios from "axios";
-import $ from "jquery";
 
 import { useEffect, useState } from "react";
+import { firstClick, secondClick } from '../redux/getMemberID.js';
+// import { CompareSharp } from '@material-ui/icons';
 
 // 임시 로컬주소
-const URL = "http://localhost:5000";
+const URL = 'http://localhost:5000'
 
-function Header() {
-  //로그인 했을때 버튼:logout으로 바꾸기
-  const [loginBtn, setLoginBtn] = useState("login");
+function Header({getID}) {
+    //로그인 했을때 버튼:logout으로 바꾸기
+    const [loginBtn, setLoginBtn] = useState("login")
 
-  const remainToken = localStorage.length
+    const remainToken = localStorage.length
         useEffect(() =>{
             if (remainToken == 1){
             setLoginBtn("logout")
             }
         })
-  
-  // 로그인 상태 판별
-  function LoginOrOut() {
-    const ValToken = window.Kakao.Auth.getAccessToken();
-    ValToken == null ? kakaoLogin() : kakaoLogout();
-  }
+    
 
-  // 카카오 로그아웃
-  function kakaoLogout() {
-    if (!window.Kakao.Auth.getAccessToken()) {
-      alert("Not logged in.");
-      return;
+    // 로그인 상태 판별
+    function LoginOrOut (){
+        const ValToken = window.Kakao.Auth.getAccessToken();
+        ValToken == null ? kakaoLogin() : kakaoLogout();
     }
-    window.Kakao.Auth.logout(function () {
-      alert("logout ok\naccess token -> " + window.Kakao.Auth.getAccessToken());
-      //usestate 변경
-      setLoginBtn("login");
-    });
-    console.log(window.Kakao.Auth.getAccessToken());
-  }
 
-  // id GET
-  // 보낼 데이터를 URL에 넣음
-  const member = async (email) => {
-    const {
-      data: { member_id },
-    } = await axios.get(`${URL}/login?email_give=${email}`);
-    console.log(member_id);
-  };
-
-  // kakao login api
-  function kakaoLogin() {
-    window.Kakao.Auth.login({
-      scope: "profile_nickname, profile_image", //동의항목 페이지에 있는 개인정보 보호 테이블의 활성화된 ID값을 넣습니다.
-      success: function (response) {
-        console.log(response); // 로그인 성공하면 받아오는 데이터
-        window.Kakao.API.request({
-          // 사용자 정보 가져오기
-          url: "/v2/user/me",
-          success: (res) => {
-            const kakao_account = res.kakao_account;
-            console.log(kakao_account.email);
-            console.log(kakao_account.profile.nickname);
-            console.log(kakao_account.profile.profile_image_url);
-
-            const email = kakao_account.email;
-            const nickname = kakao_account.profile.nickname;
-            const profile_img = kakao_account.profile.profile_image_url;
-
-            //axios 이용하여 Backend 로 보내기.
-            axios
-              .post(`${URL}/login`, {
-                email_give: email,
-                name_give: nickname,
-                img_give: profile_img,
-              })
-              .then((res) => {
-                console.log(res);
-                alert("성공");
-                // history.push("/main/feed");
-              })
-              .catch((error) => {
-                // console.log(error);
-                console.error(error);
-                alert("카카오 로그인 에러");
-              });
-
-            // id GET 호출
-            member(email);
-            // 버튼 글자 바꾸기
-            setLoginBtn("logout");
-          },
-          fail: function (error) {
-            console.log(error);
-          },
-        });
-        // window.location.href='/' //리다이렉트 되는 코드
-      },
-      fail: function (error) {
-        console.log(error);
-      },
-    });
-  }
-
-  return (
-    <div>
-      <div className={styles.menu_box} id="menu_bar">
-        <ul>
-          <li>
-            <h1 className={styles.logo}>
-              <Link to="/">
-                <span id={styles.emogi}>🏝 </span>
-                모히또에서 몰디브 한 잔
-              </Link>
-            </h1>
-          </li>
-          <li>
-            <Link to="/find">칵테일 검색</Link>
-          </li>
-          <li>
-            <Link to="/home">고향 칵테일</Link>
-          </li>
-          <li>
-            <Link to="/storage">내 칵테일 창고</Link>
-          </li>
-          <li>
-            <div className={styles.login} onClick={LoginOrOut}>
-              <span className={styles.loginText}>{loginBtn}</span>
-              {/* <AccountCircleOutlinedIcon sx={{ fontSize: 30 }} className={styles.loginIcon} /> */}
-            </div>
-          </li>
-        </ul>
-      </div>
-      {/* 스크롤업 화살표 */}
-      <div
-        onClick={() =>
-          window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-          })
+    // 카카오 로그아웃
+    function kakaoLogout() {
+        if (!window.Kakao.Auth.getAccessToken()) {
+          alert('Not logged in.')
+          return
         }
-        className={styles.scroll}
-      >
-        <img src="arrow-up-circle.svg"></img>
-      </div>
-    </div>
-  );
+        window.Kakao.Auth.logout(function() {
+          alert('logout ok\naccess token -> ' + window.Kakao.Auth.getAccessToken())
+          //usestate 변경
+          setLoginBtn("login")
+        })
+        console.log(window.Kakao.Auth.getAccessToken())
+    }
+    // /
+
+        
+    // id GET
+    // 보낼 데이터를 URL에 넣음
+    const member = async (email) => {
+        const {data:{member_id}} = await axios.get(`${URL}/login?email_give=${email}`);
+        // getID({member_id})
+        console.log(getID({member_id}))
+
+    };
+
+    // kakao login api
+    function kakaoLogin() {
+        window.Kakao.Auth.login({
+            scope: 'profile_nickname, profile_image', //동의항목 페이지에 있는 개인정보 보호 테이블의 활성화된 ID값을 넣습니다.
+            success: function (response) {
+                console.log(response) // 로그인 성공하면 받아오는 데이터
+                window.Kakao.API.request({ // 사용자 정보 가져오기
+                    url: '/v2/user/me',
+                    success: (res) => {
+                        const kakao_account = res.kakao_account;
+                        console.log(res)
+                        console.log(kakao_account.email);
+                        console.log(kakao_account.profile.nickname);
+                        console.log(kakao_account.profile.profile_image_url);
+
+                        const email = kakao_account.email
+                        const nickname = kakao_account.profile.nickname
+                        const profile_img = kakao_account.profile.profile_image_url
+                        // member();
+                        // console.log(member())
+                        //axios 이용하여 Backend 로 보내기.
+                        axios.post(
+                            `${URL}/login`,
+                            {
+                                email_give: email,
+                                name_give: nickname,
+                                img_give: profile_img,
+
+                            })
+                            .then((res) => {
+                                console.log(res);
+                                alert("성공");
+                                // history.push("/main/feed");
+                            })
+                            .catch((error) => {
+                                // console.log(error);
+                                console.error(error);
+                                alert("카카오 로그인 에러");
+                            });
+
+                        // id GET 호출    
+                        member(email);
+                        // 버튼 글자 바꾸기
+                        setLoginBtn("logout")
+
+                    },
+                    fail: function (error) {
+                        console.log(error);
+
+                    }
+                });
+                // window.location.href='/' //리다이렉트 되는 코드
+                
+
+            },
+            fail: function (error) {
+                console.log(error);
+            }
+        })
+    };
+
+
+    return (
+        <div>
+            <div className={styles.menu_box}>
+                <ul>
+                    <li>
+                    <h1 className={styles.logo}>
+                    <Link to="/">
+                        <span id={styles.emogi}>🏝 </span>
+                        모히또에서 몰디브 한 잔</Link></h1>
+                    </li>
+                    <li>
+                        <Link to="/find">칵테일 검색</Link>
+                    </li>
+                    <li>
+                        <Link to="/home">고향 칵테일</Link>
+                    </li>
+                    <li>
+                        <Link to="/storage">내 칵테일 창고</Link>
+                    </li>
+                    <li>
+                        <div className={styles.login} onClick={LoginOrOut}>
+                            <span className={styles.loginText}>{loginBtn}</span>
+                            {/* <AccountCircleOutlinedIcon sx={{ fontSize: 30 }} className={styles.loginIcon} /> */}
+                        </div>
+
+
+                    </li>
+                </ul>
+            </div>
+            {/* 스크롤업 화살표 */}
+            <div className={styles.scroll}>
+                <img src="arrow-up-circle.svg"></img></div>
+
+
+
+
+
+
+        </div>
+    );
 }
-export default Header;
+
+function mapDispatchToProps(dispatch) {
+    
+    return{
+        getID: member_id => dispatch(firstClick(member_id)),
+        removeID: state => dispatch(secondClick(state))
+    }
+};
+
+export default connect(null, mapDispatchToProps)(Header);
