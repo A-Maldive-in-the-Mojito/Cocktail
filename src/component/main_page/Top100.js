c5731067509b3e2ddeafdad2458c730d5b9edfd3
skdoqj <80323356+skdoqj@users.noreply.github.com>
2022-06-27 23:28:01 +0900

    6.27

--- src/component/main_page/Top100.js
+++ src/component/main_page/Top100.js
@@ -17,28 +17,35 @@ import 'swiper/css/autoplay'; //자동 넘기기
 //contect
 import { useContext } from 'react';
 import { APIContext } from '../../context/APIContext';
+import axios from "axios";
+
 
 function Top100() {
-    const API = useContext(APIContext);
-    // console.log(API[0].S3_img);
-    
-    const makeRandom = () => {
-        const randomArray = [];
-        for (let i = 0; i < 5; i++) {
-            //랜덤한 숫자 뽑기
-            const random = Math.floor(Math.random() * API.length);
-            const randomCocktail = API[random]
-            // console.log(randomCocktail)
-            randomArray.push(randomCocktail);   
-        }
-        console.log(randomArray);
-        // console.log(randomArray.map(item => item.S3_img))
 
-    };
-    makeRandom()
-    // useEffect(()=> {
-    //     makeRandom()
-    // },[])
+    const API = useContext(APIContext)
+
+    const TOP100 = API.filter(item => item.rank !== "no info")
+
+    const randomArray = [];
+    let n = 0
+    while (n < 5) {
+        const random = parseInt(Math.floor(Math.random() * 100));
+        console.log(random)
+        const random100 = TOP100[random]
+        console.log(random100)
+        randomArray.push(random100);
+        n++
+    }
+    console.log(randomArray);
+
+
+
+    const randomCocktail = (TOP100) => {
+        console.log(randomArray)
+        for (let num in randomArray) {
+            console.log(num)
+        }
+    }
 
 
     // let swiper = new Swiper()
@@ -51,6 +58,8 @@ function Top100() {
         <div className={mainStyles.Top100}>
             <h1>World's Top 100
                 <p>Cocktails</p></h1>
+
+            {/* <Random /> */}
 
             <Swiper className={mainStyles.swiper}
                 // install Swiper modules
@@ -78,18 +87,22 @@ function Top100() {
                             // classname 바꾸기
                         )}
                     </SwiperSlide> */}
-                    <SwiperSlide className={slide}>
-                        {/* {randomArray.map(val=> <img src={val.img}/>)} */}
 
-                    </SwiperSlide>
-                    <SwiperSlide className={slide}>
-                        <img src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
-                    </SwiperSlide>
+                    {/* //맵 */}
+                    {/* {randomArray.map(item=>
+                        <SwiperSlide key={item._id.$oid} className={mainStyles.slide}>
+                            <img src={item.S3_img}/>
+                        </SwiperSlide>
+                        )} */}
+                    {randomArray[0] != undefined ? randomArray.map(item =>
+                        <SwiperSlide className={mainStyles.slide}>
+                            <img src={item.S3_img}/>
+                        </SwiperSlide>
+                    ) : <div></div>}
+
+
                     <SwiperSlide className={mainStyles.slide}>
-                        <img src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
-                    </SwiperSlide>
-                    <SwiperSlide className={mainStyles.slide}>
-                        <img src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
+                        <img src="https://cdn.diffords.com/contrib/stock-images/2015/12/06/2015bc30dbef6fb6d903330c18edf4af1463.jpg" />
                     </SwiperSlide>
                     <SwiperSlide className={mainStyles.slide}>
                         <img src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
@@ -97,8 +110,12 @@ function Top100() {
                 </div>
 
             </Swiper>
+
+
+
             <div className={mainStyles.more}>
                 <span>+ Top 100 Cocktails 더보기</span>
+
             </div>
         </div>
 
