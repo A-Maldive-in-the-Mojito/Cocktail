baa07f848414816bea425f42633286b97d238da5
skdoqj <80323356+skdoqj@users.noreply.github.com>
2022-07-25 22:08:27 +0900

    7.25

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,133 +1,168 @@
-import cardStyles from './Card.module.css'
+import cardStyles from "./Card.module.css";
 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import { useState, useEffect, useContext, useRef } from 'react';
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import { useState, useEffect, useContext, useRef } from "react";
 import axios from "axios";
 
 import { connect, useSelector } from "react-redux";
-import { Construction } from '@mui/icons-material';
-import { getStore, removeStore } from "../redux/getStore.js"
-
+import { Construction } from "@mui/icons-material";
+import { getStore, removeStore } from "../redux/getStore.js";
 
 function Card({ id, img, name, dispatchGetStore }) {
-  const URL = 'http://localhost:5000'
+  const URL = "http://localhost:5000";
   // 리덕스 email, store 값 가져오기
-  const reduxState = useSelector(state => state)
-  const email = reduxState.email
-  const storeCocktail = reduxState.store
+  const reduxState = useSelector((state) => state);
+  const email = reduxState.email;
+  const storeCocktail = reduxState.store;
   //onClick 시 db member 의 store 값 가져오기 axios
   const getMemberInfo = async () => {
     const response = await axios.get(`${URL}/login?email_give=${email}`);
     const memberInfo = JSON.parse(response.data.member_info);
-    const DBstoreCocktail = memberInfo[0].store
-    console.log(DBstoreCocktail);  
-    dispatchGetStore(DBstoreCocktail)
+    const DBstoreCocktail = memberInfo[0].store;
+    console.log(DBstoreCocktail);
+    dispatchGetStore(DBstoreCocktail);
   };
 
   // 렌더링용 useState
   const [render, setRender] = useState(false);
   // 데이터변경 useRef
   const checked = useRef(false);
-    
+
   // 로그인체크 및 로그인/아웃에 따른 별표표기 함수 및
   // redux store 값에 반응하는 useEffect
-      function If() {
-        if(storeCocktail !== null){
-          return(
-            storeCocktail.map(res => 
-              res === name ? (setRender(true), checked.current=true):null
-              )
-            )
-        } else {
-          return (
-            setRender(false),
-            checked.current=false
-          )
-        }
-      }
-      
-  useEffect(() => If(), [storeCocktail])
+  function If() {
+    if (storeCocktail !== null) {
+      return storeCocktail.map((res) =>
+        res === name ? (setRender(true), (checked.current = true)) : null
+      );
+    } else {
+      return setRender(false), (checked.current = false);
+    }
+  }
 
-  
-  function onClick(){
+  useEffect(() => If(), [storeCocktail]);
+
+  function onClick() {
     //로그인 상태 판별 토큰 가져오기
     const ValToken = window.Kakao.Auth.getAccessToken();
     if (ValToken == null) {
-      alert("로그인 먼저 해주세요😎")
+      alert("로그인 먼저 해주세요😎");
     } else {
-    checked.current = !checked.current;
-    //별표렌더링 및 db 에 post
-    setRender((current) => !current);
-    axiosPost();
-    //redux store 값
-    console.log(storeCocktail)
+      checked.current = !checked.current;
+      //별표렌더링 및 db 에 post
+      setRender((current) => !current);
+      axiosPost();
+      //redux store 값
+      console.log(storeCocktail);
     }
-  };
+  }
 
   const axiosPost = () => {
-    axios.post(
-      `${URL}/favourite`,
-      {
-          email_give: email,
-          name_give: name,
-          checked_give: (checked.current? parseInt(1) : parseInt(0))
+    axios
+      .post(`${URL}/favourite`, {
+        email_give: email,
+        name_give: name,
+        checked_give: checked.current ? parseInt(1) : parseInt(0),
       })
       .then((res) => {
-          console.log(res);
-          alert("통신성공");
+        console.log(res);
+        alert("통신성공");
       })
       .catch((error) => {
-          // console.log(error);
-          console.error(error);
+        // console.log(error);
+        console.error(error);
       });
-      setTimeout(() => getMemberInfo(), 500);
+    setTimeout(() => getMemberInfo(), 500);
   };
 
-
-  const [hover, setHover] = useState(0);
-
+  const [cardHover, setCardHover] = useState(0);
+  const [starHover, setStarHover] = useState(0);
 
   return (
-    <div className={cardStyles.card}>
-      {/* {hover == 0 ? "" : <StarBorderIcon onClick={onClick} className={cardStyles.star_icon} />}       */}
-      <StarBorderIcon onClick={() => {onClick()}} className={`${ render ? cardStyles.true_star_icon : cardStyles.false_star_icon}`}  />
+    // 임시 render fale 빼기
+    <div
+      className={cardStyles.card}
+      onMouseOver={() => setCardHover(1)}
+      onMouseOut={() => setCardHover(0)}
+    >
+      {render == 1 ? (
+        <img
+          className={cardStyles.star_clicked}
+          onClick={onClick}
+          src="star.png"
+        />
+      ) : cardHover == 0 ? (
+        ""
+      ) : (
+        <img
+          onClick={onClick}
+          className={cardStyles.star_unclick}
+          src="star.png"
+        />
+      )}
+
+      {/* {cardHover == 0 ? (
+        ""
+      ) : render ? (
+        <img
+          className={cardStyles.star_clicked}
+          onClick={onClick}
+          src="star_clicked.png"
+        />
+      ) : starHover == 1 ? (
+        <img
+          onClick={onClick}
+          className={cardStyles.star_unclick}
+          onMouseOver={() => setStarHover(1)}
+          onMouseOut={() => setStarHover(0)}
+          src="star_unclick.png"
+        />
+      ) : (
+        <img
+          className={cardStyles.star_clicked}
+          onClick={onClick}
+          src="star_clicked.png"
+        />
+      )} */}
+      {/* <img className={cardStyles.star_clicked} src="star_clicked.png" /> */}
+      {/* <StarBorderIcon
+        onClick={() => {
+          onClick();
+        }}
+        className={`${
+          render ? cardStyles.true_star_icon : cardStyles.false_star_icon
+        }`}
+      /> */}
       <Link to={`/desc:${id}`}>
-        <div
-          onMouseOver={() => setHover(1)}
-          onMouseOut={() => setHover(0)}
-          className={cardStyles.imgContainer}>
+        <div className={cardStyles.imgContainer}>
           <img className={cardStyles.imgCocktail} src={img} />
           <h3 className={cardStyles.cocktailName}>{name}</h3>
         </div>
       </Link>
-
-
 
       {/* 
       <div className={styles.imgContainer}>
         <img className={styles.imgCocktail} src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png" />
         <h3 className={styles.cocktailName}> Cocktail </h3>
       </div> */}
-
-
     </div>
-  )
+  );
 }
 
 Card.propTypes = {
   id: PropTypes.string.isRequired,
   img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
 };
 
 function mapDispatchToProps(dispatch) {
   return {
-      dispatchGetStore: array => dispatch(getStore(array))
+    dispatchGetStore: (array) => dispatch(getStore(array)),
   };
 }
 
-export default connect(null, mapDispatchToProps) (Card);
+export default connect(null, mapDispatchToProps)(Card);
--- src/component/Find.js
+++ src/component/Find.js
@@ -1,251 +1,275 @@
-import appStyles from '../App.module.css' // ../은 상위폴더
-import Styles from './Find.module.css'
-import SearchIcon from '@mui/icons-material/Search';
-import cardStyles from './Card.module.css'
-import Card from './Card';
+import appStyles from "../App.module.css"; // ../은 상위폴더
+import Styles from "./Find.module.css";
+import SearchIcon from "@mui/icons-material/Search";
+import cardStyles from "./Card.module.css";
+import Card from "./Card";
 // import HashTag from './HashTag';
 
-import { Link } from 'react-router-dom';
-import { useEffect, useState, useTransition } from "react";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 // 리덕스
-import { useSelector, connect } from 'react-redux';
+import { useSelector, connect } from "react-redux";
 // 리덕스 액션
-import { getStore } from "../redux/getStore.js"
+import { getStore } from "../redux/getStore.js";
 import axios from "axios";
 
-
 //contect
-import { useContext } from 'react';
-import { APIContext } from '../context/APIContext';
-import { val } from 'dom7';
+import { useContext } from "react";
+import { APIContext } from "../context/APIContext";
+import { val } from "dom7";
 
-const URL = 'http://localhost:5000'
+const URL = "http://localhost:5000";
 
 function Find({ dispatchGetStore }) {
-    // 칵테일 즐겨찾기 데이터 get
-    // 이메일 가져오기
-    const reduxState = useSelector(state => state);
-    const email = reduxState.email.email
+  // 칵테일 즐겨찾기 데이터 get
+  // 이메일 가져오기
+  const reduxState = useSelector((state) => state);
+  const email = reduxState.email.email;
 
-    const getMemberInfo = async (email) => {
-        const response = await axios.get(`${URL}/login?email_give=${email}`);
-        const memberInfo = JSON.parse(response.data.member_info);
-        const storeCocktail = memberInfo[0]
-        console.log(storeCocktail);
+  const getMemberInfo = async (email) => {
+    const response = await axios.get(`${URL}/login?email_give=${email}`);
+    const memberInfo = JSON.parse(response.data.member_info);
+    const storeCocktail = memberInfo[0];
+    console.log(storeCocktail);
 
-        dispatchGetStore(storeCocktail);
-    };
+    dispatchGetStore(storeCocktail);
+  };
 
-    useEffect(() => {
-        getMemberInfo(email);
-    }, [])
+  useEffect(() => {
+    getMemberInfo(email);
+  }, []);
 
+  //context API받기
+  const API = useContext(APIContext);
 
+  // console.log(API.filter(val=> val.ingredients["재료"].includes("Gin")))
+  // for(let i = 0; i < 7; i++){
+  // console.log(API.filter(val=> val.ingredients[i]["재료"].toLowerCase().includes("gin")))
+  // }
+  const filterIngredient = API.map((val) =>
+    val.ingredients.map((val) => val["재료"])
+  )
+    .map((val) => val.map((val) => val.toLowerCase().includes("rum")))
+    .map((val) => val.includes(true));
 
-    const [isPending, startTransition] = useTransition()
-    //context API받기
-    const API = useContext(APIContext);
+  // const filterI = API.map(val=> val.ingredients.map(val=> val["재료"].map(val=> val.map(val=> val.toLowerCase()))))
+  // const filterxAPI = API.map(val => val.ingredients.map(val=> val["재료"]))
+  // console.log(filterxAPI)
+  // const aa = API.map(val=> val.ingredients)
+  // console.log(API.filter(val=> val.ingredients[0]["재료"]))
 
-    // console.log(API.filter(val=> val.ingredients["재료"].includes("Gin")))
-    // for(let i = 0; i < 7; i++){
-    // console.log(API.filter(val=> val.ingredients[i]["재료"].toLowerCase().includes("gin")))
-    // }
-    const filterIngredient = API.map(val => val.ingredients.map(val => val["재료"])).map(val => val.map(val => val.toLowerCase().includes("rum"))).map(val => val.includes(true))
+  //
 
-    // const filterI = API.map(val=> val.ingredients.map(val=> val["재료"].map(val=> val.map(val=> val.toLowerCase()))))
-    // const filterxAPI = API.map(val => val.ingredients.map(val=> val["재료"]))
-    // console.log(filterxAPI)
-    // const aa = API.map(val=> val.ingredients)
-    // console.log(API.filter(val=> val.ingredients[0]["재료"]))
+  //해시태그Array
+  const [hashArray, setHashArray] = useState([]);
+  // console.log(hashArray);
+  const getHashElements = document.getElementsByName("check");
 
+  // 해시태그 checked 판별
+  const getHash = Array.prototype.slice.call(getHashElements);
+  // console.log(getHash)
 
+  const hashTagTrueFalseArray = [];
+  getHash.map((val) => hashTagTrueFalseArray.push(val.checked));
+  // console.log(hashTagTrueFalseArray.includes(true))
 
+  const onChangeCheckbox = (event) => {
+    const hashValue = event.target.value;
 
-
-    // 
-
-    //해시태그Array
-    const [hashArray, setHashArray] = useState([])
-    // console.log(hashArray);
-    const getHashElements = document.getElementsByName("check")
-
-    // 해시태그 checked 판별
-    const getHash = Array.prototype.slice.call(getHashElements);
-    // console.log(getHash)
-
-    const hashTagTrueFalseArray = []
-    getHash.map(val => hashTagTrueFalseArray.push(val.checked))
-    // console.log(hashTagTrueFalseArray.includes(true))
-
-
-
-    const onChangeCheckbox = (event) => {
-        const hashValue = event.target.value
-
-        for (let tag of getHash) {
-            if (tag.value != hashValue) {
-                tag.checked = false
-            }
-        }
-
-
-        for (var i = 0; i < 4; i++) {
-            const findHash0 = API.filter(item => item.hashtag[0] === hashValue);
-            const findHash1 = API.filter(item => item.hashtag[1] === hashValue);
-            const findHash2 = API.filter(item => item.hashtag[2] === hashValue);
-            const findHash3 = API.filter(item => item.hashtag[3] === hashValue);
-            // 노가다로 합치기~~!
-            const allHash = findHash0.concat(findHash1).concat(findHash2).concat(findHash3)
-
-            setHashArray(allHash);
-            // console.log(allHash)
-        }
-
+    for (let tag of getHash) {
+      if (tag.value != hashValue) {
+        tag.checked = false;
+      }
     }
 
-    //검색기능
-    const [searchText, setSearchText] = useState("");
-    const onChange = (event) => {
-        setSearchText(event.target.value);
-        // console.log(searchText);
-    };
+    for (var i = 0; i < 4; i++) {
+      const findHash0 = API.filter((item) => item.hashtag[0] === hashValue);
+      const findHash1 = API.filter((item) => item.hashtag[1] === hashValue);
+      const findHash2 = API.filter((item) => item.hashtag[2] === hashValue);
+      const findHash3 = API.filter((item) => item.hashtag[3] === hashValue);
+      // 노가다로 합치기~~!
+      const allHash = findHash0
+        .concat(findHash1)
+        .concat(findHash2)
+        .concat(findHash3);
 
-    //검색창 비우기
-    const onSubmit = (event) => {
-        event.preventDefault();
-        setSearchText("");
-    };
+      setHashArray(allHash);
+      // console.log(allHash)
+    }
+  };
 
-    const hashTagArray = [
-        {
-            "name": "#TOP 100🏆",
-            "value": "top100",
-            "key": 1
-        },
-        {
-            "name": "#홈파티🏡",
-            "value": "house-party",
-            "key": 2
-        },
-        {
-            "name": "#데일리",
-            "value": "allseason-classics",
-            "key": 3
-        },
-        {
-            "name": "#산타랑_건배🎅🏻",
-            "value": "christmas",
-            "key": 4
-        },
-        {
-            "name": "#무비나잇🎬",
-            "value": "movie-nights",
-            "key": 5
-        },
+  //검색기능
+  const [searchText, setSearchText] = useState("");
+  const onChange = (event) => {
+    setSearchText(event.target.value);
+    // console.log(searchText);
+  };
 
-        {
-            "name": "#해피뉴이어🎆",
-            "value": "new-years-eve",
-            "key": 6
-        },
-        {
-            "name": "#불금🌈",
-            "value": "downtown",
-            "key": 7
-        },
-        {
-            "name": "#HBD🎂",
-            "value": "birthday",
-            "key": 8
-        },
-        {
-            "name": "#나를위한시️간🕯",
-            "value": "time-for-you",
-            "key": 9
-        },
-        {
-            "name": "#발렌타인데이🍷",
-            "value": "valentines-day",
-            "key": 10
-        },
-        {
-            "name": "#뜨밤🔥",
-            "value": "anniversary",
-            "key": 11
-        },
+  //검색창 비우기
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setSearchText("");
+  };
 
-    ]
+  const hashTagArray = [
+    {
+      name: "#TOP 100🏆",
+      value: "top100",
+      key: 1,
+    },
+    {
+      name: "#홈파티🏡",
+      value: "house-party",
+      key: 2,
+    },
+    {
+      name: "#데일리",
+      value: "allseason-classics",
+      key: 3,
+    },
+    {
+      name: "#산타랑_건배🎅🏻",
+      value: "christmas",
+      key: 4,
+    },
+    {
+      name: "#무비나잇🎬",
+      value: "movie-nights",
+      key: 5,
+    },
 
-    const [selectValue, setSelectValue] = useState();
-    const selectOnChange = (event) =>{
-        setSelectValue(event.target.value);
-        console.log(event.target.value);
-    };
+    {
+      name: "#해피뉴이어🎆",
+      value: "new-years-eve",
+      key: 6,
+    },
+    {
+      name: "#불금🌈",
+      value: "downtown",
+      key: 7,
+    },
+    {
+      name: "#HBD🎂",
+      value: "birthday",
+      key: 8,
+    },
+    {
+      name: "#나를위한시️간🕯",
+      value: "time-for-you",
+      key: 9,
+    },
+    {
+      name: "#발렌타인데이🍷",
+      value: "valentines-day",
+      key: 10,
+    },
+    {
+      name: "#뜨밤🔥",
+      value: "anniversary",
+      key: 11,
+    },
+  ];
 
+  const [selectValue, setSelectValue] = useState();
+  const selectOnChange = (event) => {
+    setSelectValue(event.target.value);
+    console.log(event.target.value);
+  };
 
+  return (
+    <div className={Styles.Find}>
+      {/* 상단 */}
+      <div>
+        {/* 검색창 */}
+        <div className={Styles.search_space}>
+          <h3>칵테일 이름 또는 재료를 검색해보세요</h3>
+          {/* 셀렉트박스 */}
+          <div className={Styles.select_box}>
+            <select onChange={selectOnChange}>
+              <option value="name">이름검색</option>
+              <option value="ingredient">재료검색</option>
+            </select>
+          </div>
 
-    return (
-        <div className={Styles.Find}>
-            {/* 상단 */}
-            <div>
-                {/* 검색창 */}
-                <div className={Styles.search_space}>
-                    <h3>칵테일 이름 또는 재료를 검색해보세요</h3>
-                    <form onSubmit={onSubmit} className={Styles.search_box}>
-                        <input onChange={onChange} className={Styles.search_input} type="text" value={searchText} />
-                        <select onChange={selectOnChange}>
-                            <option value="name">이름검색</option>
-                            <option value="ingredient">재료검색</option>
-                        </select>
-                        <i ><SearchIcon className={Styles.search_btn} /></i>
-                    </form>
-                </div>
+          <form onSubmit={onSubmit} className={Styles.search_box}>
+            <input
+              onChange={onChange}
+              className={Styles.search_input}
+              type="text"
+              value={searchText}
+            />
 
-                {/* 해시태그 */}
-                <div className={Styles.tags_box}>
-                    {hashTagArray.map(val =>
-                        <label>
-                            <input type="checkbox" name="check" onChange={onChangeCheckbox} value={val.value} className={Styles.cBox} />
-                            <span className={
-                                (val.value == "top100" ? `${Styles.TOP} ${Styles.hashtag}` : Styles.hashtag)
-                            }>{val.name}</span>
-                        </label>
-                    )}
-                </div>
-            </div>
-
-            {/* 하단 */}
-            <div className={Styles.cardContainer}>
-                {
-                    (hashTagTrueFalseArray.includes(true) ? hashArray : API).filter((val) => {
-                        if (searchText == "") {
-                            return val
-                            } else if (selectValue == "name" && val.name.toLowerCase().includes(searchText.toLowerCase())) {
-                                    // console.log(val)
-                                return val
-                        } else if ( selectValue == "ingredient" &&
-                            val.ingredients.map(val => val["재료"].toLowerCase().includes(searchText.toLowerCase())).includes(true)
-                        ) {
-                            // console.log(val)
-                            return val
-                        }
-                    }).map((cocktail) =>
-                    (<Card
-                        key={cocktail._id.$oid}
-                        id={cocktail._id.$oid}
-                        img={cocktail.S3_img}
-                        name={cocktail.name}
-                    />
-                    ))
-                }
-            </div>
+            <i>
+              <SearchIcon className={Styles.search_btn} />
+            </i>
+          </form>
         </div>
-    );
+
+        {/* 해시태그 */}
+        <div className={Styles.tags_box}>
+          {hashTagArray.map((val) => (
+            <label>
+              <input
+                type="checkbox"
+                name="check"
+                onChange={onChangeCheckbox}
+                value={val.value}
+                className={Styles.cBox}
+              />
+              <span
+                className={
+                  val.value == "top100"
+                    ? `${Styles.TOP} ${Styles.hashtag}`
+                    : Styles.hashtag
+                }
+              >
+                {val.name}
+              </span>
+            </label>
+          ))}
+        </div>
+      </div>
+
+      {/* 하단 */}
+      <div className={Styles.cardContainer}>
+        {(hashTagTrueFalseArray.includes(true) ? hashArray : API)
+          .filter((val) => {
+            if (searchText == "") {
+              return val;
+            } else if (
+              selectValue == "name" &&
+              val.name.toLowerCase().includes(searchText.toLowerCase())
+            ) {
+              // console.log(val)
+              return val;
+            } else if (
+              selectValue == "ingredient" &&
+              val.ingredients
+                .map((val) =>
+                  val["재료"].toLowerCase().includes(searchText.toLowerCase())
+                )
+                .includes(true)
+            ) {
+              // console.log(val)
+              return val;
+            }
+          })
+          .map((cocktail) => (
+            <Card
+              key={cocktail._id.$oid}
+              id={cocktail._id.$oid}
+              img={cocktail.S3_img}
+              name={cocktail.name}
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
 function mapDispatchToProps(dispatch) {
-    return {
-        dispatchGetStore: array => dispatch(getStore(array))
-    };
+  return {
+    dispatchGetStore: (array) => dispatch(getStore(array)),
+  };
 }
 
 export default connect(null, mapDispatchToProps)(Find);
