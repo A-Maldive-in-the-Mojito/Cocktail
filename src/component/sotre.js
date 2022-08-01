0d62223eae18e4e8c9b70df8281a5bd7eccbd1ef
jakebaebaek <87072502+jakebaebaek@users.noreply.github.com>
2022-08-01 20:45:14 +0900

    Delete sotre.js

--- src/component/sotre.js
+++
@@ -1,9 +0,0 @@
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-// API GET
-const getCocktails = async() => {
-  const {data: {all_cocktails}} = await axios.get('http://localhost:5000/cocktails');
-  console.log(all_cocktails);
-};
-useEffect(()=> { getCocktails()} );
