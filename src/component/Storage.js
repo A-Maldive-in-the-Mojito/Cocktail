03d08e9a23f792c13937638c41be7c17b5c108ce
jakebaebaek <87072502+jakebaebaek@users.noreply.github.com>
2022-08-09 22:15:27 +0900

    0809 내 칵테일 창고 css 개선

--- src/component/Storage.js
+++ src/component/Storage.js
@@ -1,20 +1,14 @@
 import styles from './Storage.module.css' // ../은 상위폴더
-import { Link } from 'react-router-dom'
 import Card from './Card.js'
-import axios from "axios"
-import $ from "jquery";
 import { useEffect } from "react";
 
-import { Provider, useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 //contect
 import { useContext } from 'react';
 import { APIContext } from '../context/APIContext';
-import { store } from '../redux/store';
-import storage from 'redux-persist/lib/storage';
 const URL = 'http://localhost:5000'
 
-// 백에서 토큰 받아오는 axios
 function Storage() {
 
     //context 모든 칵테일 API
@@ -23,7 +17,7 @@ function Storage() {
     console.log(useID);
 
     const reduxState = useSelector(state => state);
-    const storeCocktail = reduxState.store.store
+    const storeCocktail = reduxState.store
 
     //api 정제
     const fav = API.filter((val) => (
@@ -34,21 +28,22 @@ function Storage() {
 
 
     return (
-        <div>
-            <div className='wrapper'>
-                <div className={styles.cardContainer}>
-                    {fav.map((cocktail) =>
-                    (<Card
-                        key={cocktail._id.$oid}
-                        id={cocktail._id.$oid}
-                        img={cocktail.S3_img}
-                        name={cocktail.name}
-                    />
-                    ))}
-                </div>
+        <div className={styles.wrapper}>
+            <div className={styles.cardContainer}>
+                {
+                fav.map((cocktail) =>
+                        (
+                        <Card
+                            key={cocktail._id.$oid}
+                            id={cocktail._id.$oid}
+                            img={cocktail.S3_img}
+                            name={cocktail.name}
+                        />
+                    )
+                )}
+                
             </div>
-        </div>
-
+        </div>                  
     )
 };
 export default Storage;
